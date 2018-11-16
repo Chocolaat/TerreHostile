9b9ff4356bcb39880a53d8b498b200c0c2db8108
Silvere Chouzier <silvere.chouzier@soprasteria.com>
2018-11-16 17:07:08 +0100

    [BDDOneTilePerRow]Tile class

--- TerreHostile/TH_Business/src/main/java/org/terrehostile/business/map/Tile.java
+++ TerreHostile/TH_Business/src/main/java/org/terrehostile/business/map/Tile.java
@@ -1,51 +1,66 @@
 package org.terrehostile.business.map;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+
 public class Tile {
 	
-	private BackgroundType background;
-	private int backgroundValue;
-	private OnTileObject listElems;
+	
+	@Id
+	@Column(name="xCoord", nullable = false)
+	private int xCoord;
+	
+	@Id
+	@Column(name="yCoord", nullable = false)
+	private int yCoord;
+	
+
+	@Column(name="background", nullable = false)
+	private int background;
+	@Column(name="height", nullable = false)
 	private int height;
 	
-	public Tile (BackgroundType background, int height)
+	
+	public Tile()
 	{
+		
+	}
+	
+	public Tile(int xCoord, int yCoord, int background, int height) {
+		this.xCoord = xCoord;
+		this.yCoord = yCoord;
 		this.background = background;
-		this.backgroundValue = background.getImgPositionInimageFilesJson();
 		this.height = height;
 	}
 	
-	
-
+	public int getxCoord() {
+		return xCoord;
+	}
+	public void setxCoord(int xCoord) {
+		this.xCoord = xCoord;
+	}
+	public int getyCoord() {
+		return yCoord;
+	}
+	public void setyCoord(int yCoord) {
+		this.yCoord = yCoord;
+	}
+	public int getBackground() {
+		return background;
+	}
+	public void setBackground(int background) {
+		this.background = background;
+	}
 	public int getHeight() {
 		return height;
 	}
-	
 	public void setHeight(int height) {
 		this.height = height;
 	}
-
-	public BackgroundType getBackground() {
-		return background;
-	}
-
-	public void setBackground(BackgroundType background) {
-		this.background = background;
-	}
-
-	public OnTileObject getListElems() {
-		return listElems;
-	}
-
-	public void setListElems(OnTileObject listElems) {
-		this.listElems = listElems;
-	}
-
-	public int getBackgroundValue() {
-		return backgroundValue;
-	}
-
-	public void setBackgroundValue(int backgroundValue) {
-		this.backgroundValue = backgroundValue;
-	}
 	
+	@Override
+	public String toString() {
+		return "Tile [xCoord=" + xCoord + ", yCoord=" + yCoord + ", background=" + background + ", height=" + height
+				+ "]";
+	}
 }
