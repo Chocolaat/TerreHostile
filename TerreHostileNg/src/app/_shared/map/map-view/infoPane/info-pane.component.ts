8e1f2e8357a936380666373eea158b9e355fda80
unknown <silvere.chouzier@gmail.com>
2019-03-18 22:26:44 +0100

    [Ng] Pannel that disaply coords and size

--- TerreHostileNg/src/app/_shared/map/map-view/infoPane/info-pane.component.ts
+++ TerreHostileNg/src/app/_shared/map/map-view/infoPane/info-pane.component.ts
@@ -1,18 +1,36 @@
 import { MapEditorSelection } from 'src/app/modules/admin/mapEditor/model/mapEditorSelection';
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
+import * as MapJsModule from 'src/assets/js/map/map.js';
+import { MapService } from '../../services/map.service';
+
 
 @Component({
     selector: 'app-map-info-pane',
     template: `
-  <label>X = </label>
-  <label>Y = </label>
-  <label>Size = </label>
+    <img src="assets/images/backgrounds/map/info_panel.png" id="infoPanelPicture">
+    <div id="infoPanelText">
+      <p> Position : ({{currentXCoord}}, {{currentYCoord}}) / Size : {{size}}</p>
+   </div>
     `,
     styleUrls: ['./info-pane.component.css']
   })
-export class InfoPaneComponent {
+export class InfoPaneComponent implements OnInit {
 
-    constructor() {
+  currentXCoord: number;
+  currentYCoord: number;
+  size: number;
 
+  ngOnInit() {
+
+    this.mapService.getMapByXYAndSize(500, 500, 30).subscribe(
+      () => {
+       this.currentXCoord = MapJsModule.map.currentXCoord;
+       this.currentYCoord = MapJsModule.map.currentYCoord;
+       this.size = MapJsModule.map.xSize;
+      });
+
+  }
+
+    constructor(private mapService: MapService) {
     }
 }
